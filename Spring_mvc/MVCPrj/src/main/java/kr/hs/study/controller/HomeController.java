a5d31c3e7e435af2cde741d9db5389f7a386a842
Yuna Hwang <57125986+hwangyouna03@users.noreply.github.com>
2021-07-05 19:37:55 +0900

    mvc 동작

---
+++ Spring_mvc/MVCPrj/src/main/java/kr/hs/study/controller/HomeController.java
@@ -0,0 +1,17 @@
+package kr.hs.study.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+	@RequestMapping(value="/test",method=RequestMethod.GET)
+	public String home() {
+		System.out.println("test");
+		return null;
+	}
+	
+	// test로 접속했을 때 test 출력시켜보기
+	
+}
